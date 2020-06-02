b95660d7d96b37548b73b0bbf1e98f54add8379d
Changa Imara <cdimara@gmail.com>
2020-06-02 21:58:53 +0000

    added #26

--- main.clj
+++ main.clj
@@ -4,3 +4,11 @@
 
 ; #27 - Palindrome Detector
 #(= (reverse %) (seq %)))
+
+;#26 - Fibonacci Sequence
+(fn fib 
+  ([n] (fib n 0 1 []))
+	([n a b res]
+	 (if-not (> n 0)
+	   res
+		 (recur (dec n) b (+ a b) (conj res b)))))
